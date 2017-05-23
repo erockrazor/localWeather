cf5b35d5f65b0c8e03782b6937a7735e3d858df6
Eric P. Gieg <erockusd@Hotmail.com>
2017-05-23 00:38:37 -0400

    updated fahrenheit

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
         console.log(response);
         var iconRequest = response.currently.icon;
         var temperatureCurrently = Math.round(response.currently.temperature);
-        $("#temperature").html('<h1>' + temperatureCurrently + '</h1>');
+        $("#temperature").html('<h1>' + temperatureCurrently + "° f" + '</h1>');
         $("#summary").html('<h2>' + response.currently.summary + '</h2>');
         // console.log(iconRequest);
         if (iconRequest.includes("partly-cloudy-night")) {
@@ -36,11 +36,11 @@ $(document).ready(function() {
 
         $("#celciusButton").click(function() {
           console.log(response.currently.temperature - 32 * 0.5556);
-          $("#temperature").html('<h1>' + Math.round((temperatureCurrently - 32) * 0.5556)+ " C°" +'</h1>');
+          $("#temperature").html('<h1>' + Math.round((temperatureCurrently - 32) * 0.5556)+ "° c" +'</h1>');
         });
 
         $("#fahrenheitButton").click(function() {
-          $("#temperature").html('<h1>' + Math.round(temperatureCurrently) + " F°" +'</h1>');
+          $("#temperature").html('<h1>' + Math.round(temperatureCurrently) + "° f" +'</h1>');
         });
 
       });
