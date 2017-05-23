5dd77b04db2f1a8976c2a1433e175c304f3f528d
Eric P. Gieg <erockusd@Hotmail.com>
2017-05-23 11:46:38 -0400

    added wait directions

--- javascript.js
+++ javascript.js
@@ -8,14 +8,11 @@ $(document).ready(function() {
       $("#geoLocation").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
       var latitude = position.coords.latitude;
       var longitude = position.coords.longitude;
-      console.log(latitude + longitude);
       $.getJSON("https://crossorigin.me/https://api.darksky.net/forecast/ebcbd99d8eb007ce61196e23b91769a4/" + latitude + "," + longitude, function(response) {
-        console.log(response);
         var iconRequest = response.currently.icon;
         var temperatureCurrently = Math.round(response.currently.temperature);
         $("#temperature").html('<h1>' + temperatureCurrently + "° f" + '</h1>');
         $("#summary").html('<h2>' + response.currently.summary + '</h2>');
-        // console.log(iconRequest);
         if (iconRequest.includes("partly-cloudy-night")) {
           ($("#icon").html('<img src="images/Cloud-Moon.svg" alt="">'));
         } else if (iconRequest.includes("clear-night")) {
@@ -34,9 +31,12 @@ $(document).ready(function() {
           ($("#icon").html('<img src="images/Cloud.svg" alt="">'));
         }
 
+
+        $("#tempIcon").html('<img id="fahrenheitButton" src="images/Degrees-Fahrenheit.svg" alt=""><img id="celciusButton" src="images/Degrees-Celcius.svg" alt="">');
+
+
         $("#celciusButton").click(function() {
-          console.log(response.currently.temperature - 32 * 0.5556);
-          $("#temperature").html('<h1>' + Math.round((temperatureCurrently - 32) * 0.5556)+ "° c" +'</h1>');
+          $("#temperature").html('<h1><b>' + Math.round((temperatureCurrently - 32) * 0.5556)+ "° c" +'</b></h1>');
         });
 
         $("#fahrenheitButton").click(function() {
